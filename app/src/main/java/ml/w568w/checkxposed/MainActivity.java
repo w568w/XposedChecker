dc7f7a1cfe7dc764c979445ea4616a776c9e5e04
w568w <1278297578@qq.com>
2018-06-22 10:09:51 +0800

    Fix the root checking

--- app/src/main/java/ml/w568w/checkxposed/MainActivity.java
+++ app/src/main/java/ml/w568w/checkxposed/MainActivity.java
@@ -119,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
                 return layout;
             }
         });
-        int checkCode = check1() + check2() + check3() + check4() + check5() + check6() + check7() + check8() + check9();
+        int checkCode = check1() + check2() + check3() + check4() + check5() + check6() + check7() + check8();
         if (checkCode > 0) {
             textView.setTextColor(Color.RED);
             textView.setText("你安装了 Xposed ! 可信度: " + checkCode + "/" + CHECK_ITEM.length);
