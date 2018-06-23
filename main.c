13971f02fe5d89a53767920a1e4875ffed46d9a0
w568w <1278297578@qq.com>
2018-06-23 15:34:58 +0800

    Add the C Native part

---
+++ main.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <stdlib.h>
+char isXposed(char * filename){
+    //strstr相当于String.indexOf()
+    return strstr(filename,"xposed")!=NULL;
+}
+long filelength(FILE *fp){
+ long num;
+ fseek(fp,0,SEEK_END);
+ num=ftell(fp);
+ fseek(fp,0,SEEK_SET);
+ return num;
+}
+char isXposedMaps(char * pid){
+	FILE * maps;
+	char path[80];
+	char * content;
+	strcpy (path,"/proc/");
+	strcat (path,pid);
+	strcat (path,"/maps");
+	if((maps=fopen(path,"r"))==NULL){
+		return 0;
+	}else{
+		int len=filelength(maps);
+		content=(char *)malloc(len);
+		fread(content,len,1,maps);
+		content[len-1]='\0';
+		return strstr(content,"XposedBridge")!=NULL;
+	}
+}
+int main(int argc,char** argv){
+	/*DIR * dir;
+	struct dirent * ptr;
+	dir = opendir("/system/lib");
+    printf("fuck the wechat and tencent.");
+	while(ptr = readdir(dir)){
+        if(isXposed(ptr->d_name)){
+			closedir(dir);
+			return 0;
+		}
+    }
+	closedir(dir);
+*/
+    if(argc==2&&isXposedMaps(argv[1]))
+		return 0;
+	return 1;
+}
